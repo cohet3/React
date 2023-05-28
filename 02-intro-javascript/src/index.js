2d9ea73b16621042d7f0b6743d533ac54f93cdd8
cohet3 <tauro.total90@gmail.com>
2023-05-28 21:08:37 +0200

    first commit

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,18 @@
+console.log('Hola WOrlD!!!');
+
+const persona= {
+    nombre:'DAnny',
+    apellido : 'Rods',
+    edad: 30,
+    direccion: {
+        ciudad: ' New York',
+        Postal:28030
+    }
+
+};
+
+
+console.log(persona)
+
+
+
