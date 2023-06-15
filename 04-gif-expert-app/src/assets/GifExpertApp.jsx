5751f9c14f85b53187b93efe3f945950abbd278b
cohet3 <tauro.total90@gmail.com>
2023-06-15 20:59:10 +0200

    continuacion del curso

---
+++ 04-gif-expert-app/src/assets/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+const GifExpertApp = () => {
+  return (
+    <>
+        <h1>GitExpertApp</h1>
+    </>
+  )
+}
+
+export default GifExpertApp
+// WiU973VA3HNP8z6ZbV5UU1carU0cGHFC
